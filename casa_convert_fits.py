f248aa68952c1647763ce4cf5685f481775c43f8
Jack Radcliffe <jack.radcliffe@postgrad.manchester.ac.uk>
2017-09-07 11:27:29 +0200

    changed_casa_convert

--- casa_convert_fits.py
+++ casa_convert_fits.py
@@ -1,8 +1,12 @@
 import os
 
 for file in os.listdir('./'):
+    if (os.path.isdir(file) == True) and ((file.endswith('.image')) or (file.endswith('.pbcor'))):
+        exportfits(imagename=file,fitsimage=file[:-6]+'_casa.fits',\
+        dropdeg=True,dropstokes=True)
     if file.endswith('.fits'):
         importfits(fitsimage=file, imagename=file[:-5]+'_casa.image')
-        exportfits(imagename=file[:-5]+'_casa.image',fitsimage=file[:-5]+'_casa.fits',dropdeg=True,dropstokes=True)
+        exportfits(imagename=file[:-5]+'_casa.image',fitsimage=file[:-5]+'_casa.fits',\
+        dropdeg=True,dropstokes=True)
         os.system('rm -r '+file[:-5]+'_casa.image')
 os.system('rm casa-*.log ipython-*.log')
